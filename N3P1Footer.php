7cffc0f21a09283a3f7418c6272eefb2ac356569
DAW2-Vicente21 <71894608+DAW2-Vicente21@users.noreply.github.com>
2020-11-30 02:54:25 +0100

    Create N3P1Footer.php

---
+++ N3P1Footer.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title> Welcome!</title>
+</head>
+<body>
+    <!-- Ejercici 2-->
+    <div style="position: relative; width: 600px; height: 800px;">    
+
+        <div style="position: absolute; bottom: 5px">
+        Site developed by: <a href="vicenteiortizxavier@fpllefia.com">vicenteiortizxavier@fpllefia.com</a>
+        </div>
+
+    </div>
+<br/>
+</div>
+</body>
+</html>
+
+
